188d244b894241a5d52f5e3ba2ebd64ed1907c3d
duhan666 <kiger-666@163.com>
2017-03-22 17:26:58 +0800

    2017-3-22

---
+++ pwd1.c
@@ -0,0 +1,31 @@
+//
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+/*
+struct stat {
+    dev_t     st_dev;     /* ID of device containing file */
+    ino_t     st_ino;     /* inode number */
+    mode_t    st_mode;    /* protection */
+    nlink_t   st_nlink;   /* number of hard links */
+    uid_t     st_uid;     /* user ID of owner */
+    gid_t     st_gid;     /* group ID of owner */
+    dev_t     st_rdev;    /* device ID (if special file) */
+    off_t     st_size;    /* total size, in bytes */
+    blksize_t st_blksize; /* blocksize for filesystem I/O */
+    blkcnt_t  st_blocks;  /* number of 512B blocks allocated */
+    time_t    st_atime;   /* time of last access */
+    time_t    st_mtime;   /* time of last modification */
+    time_t    st_ctime;   /* time of last status change */
+}; 
+*/
+
+
+char* filenameto(char* filename){
+    struct stat file_info ;
+    if(stat(filename,file_info)==-1)
+        printf("read file stat_info error!\n");
+    file_info->st_ino;
+}
