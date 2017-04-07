a1ec85f28d04da00f264c39e56977f40f547b3a5
duhan666 <kiger-666@163.com>
2017-04-07 15:36:22 +0800

    2017-4-7

--- moveboard.c
+++ moveboard.c
@@ -2,7 +2,23 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <signal.h>
+#include <curses.h>
 
+#define TOP 10
+#define BOTTOM 40
+#define L_EDGE 10
+#define R_EDGE 50
+#define BLANK ("           ")
+#define HELLO ("HE")
+
+	int h_pos = L_EDGE;
+	int v_pos = TOP;
+
+	int  delay = 500;
+	int h_dir = 1;
+	int v_dir = 1; 
+	int temp_h;
+	int temp_v;
 
 int set_timer(int msec){
 
@@ -11,25 +27,59 @@ int set_timer(int msec){
 
 	struct itimerval time_val;
 
-	time_val.it_interval.tv_sec = sec;
-	time_val.it_interval.tv_usec = u_sec;
+	time_val.it_interval.tv_sec = 1;
+	time_val.it_interval.tv_usec = 0;
 
-	time_val.it_value.tv_sec = 1;
-	time_val.it_value.tv_usec = 0;
+	time_val.it_value.tv_sec = sec;
+	time_val.it_value.tv_usec = u_sec;
 	
 	return setitimer(ITIMER_REAL,&time_val,NULL);
 }
 
-void f(int s){
-	printf("the timer is triggered!\n");
-}
+void f(int);
 
 void main(){
-	set_timer(3000);
+	initscr();
+	crmode();
+	noecho();
+	clear();
+
+	move(h_pos,v_pos);
+	addstr(HELLO);
+	set_timer(500);
+
 	signal(SIGALRM,f);
-	while(1){
-		printf("haha\n");
-		sleep(1);
+
+	int c ;
+	while((c=getchar())!=EOF){
+		
+		switch(c){
+			case 's' : delay = delay*2+1;break;
+			case 'f' : delay = delay/2+1;break;
+			case ' ' : v_dir = -v_dir;h_dir = -h_dir;break;
+			default  : break;
+		}
+		printf("%d\n", delay);
 	}
+	
 }
 
+void f(int s){
+
+	signal(SIGALRM,f);
+	move(temp_h,temp_v);
+	addstr(BLANK);
+	move(h_pos,v_pos);
+	temp_h = h_pos;
+	temp_v = v_pos;
+	addstr(HELLO);
+	refresh();
+	set_timer(delay);
+	h_pos = h_pos + h_dir;
+	v_pos = v_pos + v_dir;
+	if(h_pos>R_EDGE || h_pos<L_EDGE)
+		h_dir = -h_dir;
+	if(v_pos<TOP || v_pos>BOTTOM)
+		v_dir = -v_dir;
+	//printf("%d\t%d\n",h_pos,v_pos );
+}
--- temp.c
+++ temp.c
@@ -4,8 +4,16 @@
 #include <stdio.h>
 
 void main(){
-	char c[10];
-	c[0] = '/';
-	c[1] = '\0';
-	printf("%s\n",c);
+	int c;
+	int delay=500;
+while((c=getchar())!=EOF){
+		
+		switch(c){
+			case 's' : delay = delay/2;break;
+			case 'f' : delay = delay*2;break;
+			//case ' ' : { v_dir = -v_dir;h_dir = -h_dir;};
+			default  : break;
+		}
+		printf("%d\n",delay);
+	}
 }
