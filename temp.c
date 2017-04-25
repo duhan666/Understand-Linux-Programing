53cd578abdf99a8f967d1782cdafc27f7cf9dae1
duhan <kiger-666@163.com>
2017-04-25 01:19:20 -0700

    2017-4-25 16:19

--- temp.c
+++ temp.c
@@ -4,22 +4,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-       #include <sys/types.h>          /* See NOTES */
-       #include <sys/socket.h>
+#include <sys/types.h>        
+#include <sys/socket.h>
 #include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
 
+#define HOSTNAMELEN 30
+//#define PORTNUM 13157
 
-void main(){
-	char awp[30];
-	gethostname(awp,30);
+/*
+ struct   sockaddr_in    
+{    
+    short int           sin_family;     //2  
+    unsigned short int  sin_port;     //2  
+    struct in_addr      sin_addr;      //4  
+    unsigned   char     sin_zero[8];     //8  
+};   
 
-	struct hostent *hp;
+struct in_addr            
+{  
+    in_addr_t s_addr;   // in_addr_t一般为32位的unsigned int,其字节顺序为网络字节序，即该无符号数采用大端字节序。其中每8位表示一个IP地址中的一个数值。  
+};  
 
-	hp = gethostbyname(awp);
+struct hostent{
+    char * h_name;
+    char ** h_aliases;
+    short h_addrtype;
+    short h_length;
+    char ** h_addr_list;
+    #define h_addr h_addr_list[0];
+};    
 
+*/
 
+int create_socket(int portnum){
+	
+	char str[HOSTNAMELEN];
+	int h_name = gethostname(str,HOSTNAMELEN);
+	if(h_name == -1)printf("get host name error!\n");
+	printf("%s\n",str);
 
-	printf("%s\n",hp->h_addr);
+	struct hostent* hp = gethostbyname(str);
+	if(hp == NULL)printf("get host by name error!\n");
+
+	int sktid = socket(AF_INET,SOCK_STREAM,0);
+	if(sktid == -1)printf("socket error!\n");
+
+	struct sockaddr_in myaddr;
+	myaddr.sin_family = AF_INET;
+	myaddr.sin_port   = htons(portnum);
+	memcpy(&(myaddr.sin_addr),hp->h_addr,hp->h_length);
+	//myaddr.sin_addr.s_addr = inet_addr("192.168.13.221");
+
+	int bindid = bind(sktid,(struct sockaddr*)&myaddr,sizeof(myaddr));
+	if(bindid == -1)printf("bind error!\n");
+
+	return sktid;
 }
 
-
+void main(){
+	int skt_id = create_socket(13000);
+	printf("success!\n");
+	printf("%d\n",skt_id);
+}
--- webser.c
+++ webser.c
@@ -1,33 +1,72 @@
+//temp
 
+#include <time.h>
 #include <stdio.h>
- #include <netinet/in.h>
-       #include <sys/types.h>  
-       #include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>        
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
 
+#define HOSTNAMELEN 30
+//#define PORTNUM 13157
 
-int make_socket(){
+/*
+ struct   sockaddr_in    
+{    
+    short int           sin_family;     //2  
+    unsigned short int  sin_port;     //2  
+    struct in_addr      sin_addr;      //4  
+    unsigned   char     sin_zero[8];     //8  
+};   
+
+struct in_addr            
+{  
+    in_addr_t s_addr;   // in_addr_t一般为32位的unsigned int,其字节顺序为网络字节序，即该无符号数采用大端字节序。其中每8位表示一个IP地址中的一个数值。  
+};  
+
+struct hostent{
+    char * h_name;
+    char ** h_aliases;
+    short h_addrtype;
+    short h_length;
+    char ** h_addr_list;
+    #define h_addr h_addr_list[0];
+};    
+
+*/
+
+int create_socket(int portnum){
 	
-	int skt_fd = socket(AF_INET,SOCK_STREAM,0);
+	char str[HOSTNAMELEN];
+	int h_name = gethostname(str,HOSTNAMELEN);
+	if(h_name == -1)printf("get host name error!\n");
+	printf("%s\n",str);
 
-	struct sockaddr_in addr;
+	struct hostent* hp = gethostbyname(str);
+	if(hp == NULL)printf("get host by name error!\n");
 
-	//memset(&myipv4sock,0,sizeof(struct sockaddr_in));
+	int sktid = socket(AF_INET,SOCK_STREAM,0);
+	if(sktid == -1)printf("socket error!\n");
 
-	addr.sin_family = AF_INET;
-	addr.sin_port   = htons(13157);
-	addr.sin_addr.s_addr = inet_addr("192.168.1.103");
+	struct sockaddr_in myaddr;
+	myaddr.sin_family = AF_INET;
+	myaddr.sin_port   = htons(portnum);
+	memcpy(&(myaddr.sin_addr),hp->h_addr,hp->h_length);
+	//myaddr.sin_addr.s_addr = inet_addr("192.168.13.221");
 
+	int bindid = bind(sktid,(struct sockaddr*)&myaddr,sizeof(myaddr));
+	if(bindid == -1)printf("bind error!\n");
 
-	int bind_id = bind(skt_fd,(struct sockaddr *)&addr,sizeof(addr));
-
-	if(bind_id == -1)
-		printf("bind error\n");
-
+	return sktid;
 }
 
 void main(){
+	int skt_id = create_socket(13000);
+	printf("success!\n");
+	printf("%d\n",skt_id);
 
-	make_socket();
+
 }
-
-
